3e12cff946a1acc289f14eef11853c171f0b0eb8
Jason Hunsaker <jasonmhunsaker@gmail.com>
2022-02-17 18:11:30 -0500

    Update todomvc-actions.spec.js

--- cypress/integration/todomvc-actions.spec.js
+++ cypress/integration/todomvc-actions.spec.js
@@ -7,18 +7,15 @@ describe('todo actions', () => {
         cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
 })
     it('should add a new todo to the list', () => {
-        
-
+       
         cy.get('label').should('have.text', 'Clean room')
         cy.get('toggle').should('not.be.checked')
     })
-
 
     it('should mark the todo as completed', () => {
         cy.get('.toggle').click()
         cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
     })
-
 
     it('should clear completed todos', () => {
         cy.get('.toggle').click()
